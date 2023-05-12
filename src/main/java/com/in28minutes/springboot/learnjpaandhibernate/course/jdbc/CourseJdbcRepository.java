02149aa44ed324a0dd75489890909ed66ca08187
Tommes355 <thomas.kreuscher@gmx.net>
2023-05-12 15:07:24 +0200

    #142 start

--- src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcRepository.java
+++ src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcRepository.java
@@ -1,8 +1,11 @@
 package com.in28minutes.springboot.learnjpaandhibernate.course.jdbc;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.in28minutes.springboot.learnjpaandhibernate.course.Course;
 
 
 @Repository
@@ -14,11 +17,32 @@ public class CourseJdbcRepository {
   private static String INSERT_QUERY = 
       """
         insert into course (id, name, author)
-        values(1, 'Learn AWS', 'in28minutes');
+        values(?, ?, ?);
       """;
 
-  public void insert() {
-    springJdbcTemplate.update(INSERT_QUERY);
+  private static String DELETE_QUERY = 
+      """
+        delete from course
+        where id = ?
+      """;
+
+  private static String SELECT_QUERY = 
+      """
+        select * from course
+        where id = ?
+      """;
+
+  public void insert(Course course) {
+    springJdbcTemplate.update(INSERT_QUERY, 
+            course.getId(), course.getName(), course.getAuthor());
+  }
+
+  public void deleteById(long id) {
+    springJdbcTemplate.update(DELETE_QUERY, id);
   }
   
+  public Course findById(long id) {
+    return springJdbcTemplate.queryForObject(SELECT_QUERY, 
+            new BeanPropertyRowMapper<>(Course.class), id);
+  }
 }
