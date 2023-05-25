4ab16f9046d5e059bc22cbe19accde1fbe26bb17
Tommes355 <thomas.kreuscher@gmx.net>
2023-05-25 14:48:35 +0200

    #100 start

---
+++ src/main/java/com/in28minutes/springboot/firstrestapi/survey/SurveyResource.java
@@ -0,0 +1,67 @@
+package com.in28minutes.springboot.firstrestapi.survey;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+public class SurveyResource {
+
+    private SurveyService surveyService;
+
+    public SurveyResource(SurveyService surveyService) {
+        this.surveyService = surveyService;
+    }
+
+    @RequestMapping("/surveys")
+    public List<Survey> retrieveAllSurveys() {
+        return surveyService.retrieveAllSurveys();
+    }
+
+    @RequestMapping("/surveys/{surveyId}")
+    public Survey retrieveSurveyById(@PathVariable String surveyId) {
+        Survey survey = surveyService.retrieveSurveyById(surveyId);
+        if (survey == null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        return survey;
+    }
+
+    @RequestMapping("/surveys/{surveyId}/questions")
+    public List<Question> retrieveAllSurveyQuestions(@PathVariable String surveyId) {
+        List<Question> questions = surveyService.retrieveAllSurveyQuestions(surveyId);
+
+        if (questions == null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        return questions;
+    }
+
+
+    
+
+    @RequestMapping("/surveys/{surveyId}/questions/{questionId}")
+    public Question retrieveSpecificSurveyQuestions(@PathVariable String surveyId,
+            @PathVariable String questionId) {
+        Question question = surveyService.retrieveSpecificSurveyQuestions(surveyId, questionId);
+
+        if (question == null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        return question;
+    }
+
+    @RequestMapping(value="/surveys/{surveyId}/questions", method = RequestMethod.POST)
+    public void addNewSurveyQuestion(@PathVariable String surveyId,
+            @RequestBody Question question) {
+
+        surveyService.addNewSurveyQuestion(surveyId, question);
+    }
+
+}
