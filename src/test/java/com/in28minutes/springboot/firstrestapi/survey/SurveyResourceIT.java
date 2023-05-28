0c266a3f4c745c997564217c4525de952a3aaefa
Tommes355 <thomas.kreuscher@gmx.net>
2023-05-28 02:17:49 +0200

    #109 start

---
+++ src/test/java/com/in28minutes/springboot/firstrestapi/survey/SurveyResourceIT.java
@@ -0,0 +1,59 @@
+package com.in28minutes.springboot.firstrestapi.survey;
+
+
+import org.json.JSONException;
+import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class SurveyResourceIT {
+
+  // http://localhost:8080/surveys/Survey1/questions/Question1
+  String str = """
+      {
+        "id": "Question1",
+        "description": "Most Popular Cloud Platform Today",
+        "options": [
+        "AWS",
+        "Azure",
+        "Google Cloud",
+        "Oracle Cloud"
+        ],
+        "correctAnswer": "AWS"
+        }
+        """;
+
+  private static String SPECIFIC_QUESTION_URL = "/surveys/Survey1/questions/Question1";
+
+  @Autowired
+  private TestRestTemplate template;
+
+  // {"id":"Question1","description":"Most Popular Cloud Platform
+  // Today","options":["AWS","Azure","Google Cloud","Oracle
+  // Cloud"],"correctAnswer":"AWS"}
+  // [Content-Type:"application/json", ]
+
+  @Test
+  void retrieveSpecificSurveyQuestion_basicScenario() throws JSONException {
+    ResponseEntity<String> responseEntity = template.getForEntity(SPECIFIC_QUESTION_URL, String.class);
+    
+    String expectedResponse = """
+      {"id":"Question1",
+      "description":"Most Popular Cloud Platform Today",
+      "correctAnswer":"AWS"}
+        """;
+    
+    JSONAssert.assertEquals(expectedResponse, responseEntity.getBody(), false);    
+    // assertEquals(expectedResponse.trim(), responseEntity.getBody());
+    // System.out.println(responseEntity.getBody());
+    // System.out.println(responseEntity.getHeaders());
+
+  }
+
+
+}
