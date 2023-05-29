4bd0978bf34bfdbdb6285a086ef9f33fd2d78187
Tommes355 <thomas.kreuscher@gmx.net>
2023-05-29 22:28:49 +0200

    #112 start

--- src/test/java/com/in28minutes/springboot/firstrestapi/survey/SurveyResourceIT.java
+++ src/test/java/com/in28minutes/springboot/firstrestapi/survey/SurveyResourceIT.java
@@ -1,5 +1,7 @@
 package com.in28minutes.springboot.firstrestapi.survey;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.json.JSONException;
 import org.junit.jupiter.api.Test;
@@ -8,52 +10,94 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class SurveyResourceIT {
 
-  // http://localhost:8080/surveys/Survey1/questions/Question1
-  String str = """
-      {
-        "id": "Question1",
-        "description": "Most Popular Cloud Platform Today",
-        "options": [
-        "AWS",
-        "Azure",
-        "Google Cloud",
-        "Oracle Cloud"
-        ],
-        "correctAnswer": "AWS"
-        }
-        """;
-
   private static String SPECIFIC_QUESTION_URL = "/surveys/Survey1/questions/Question1";
+
+  private static String GENERIC_QUESTION_URL = "/surveys/Survey1/questions";
 
   @Autowired
   private TestRestTemplate template;
 
-  // {"id":"Question1","description":"Most Popular Cloud Platform
-  // Today","options":["AWS","Azure","Google Cloud","Oracle
-  // Cloud"],"correctAnswer":"AWS"}
-  // [Content-Type:"application/json", ]
-
   @Test
   void retrieveSpecificSurveyQuestion_basicScenario() throws JSONException {
     ResponseEntity<String> responseEntity = template.getForEntity(SPECIFIC_QUESTION_URL, String.class);
-    
-    String expectedResponse = """
-      {"id":"Question1",
-      "description":"Most Popular Cloud Platform Today",
-      "correctAnswer":"AWS"}
-        """;
-    
-    JSONAssert.assertEquals(expectedResponse, responseEntity.getBody(), false);    
-    // assertEquals(expectedResponse.trim(), responseEntity.getBody());
-    // System.out.println(responseEntity.getBody());
-    // System.out.println(responseEntity.getHeaders());
+
+    String expectedResponse = 
+    """
+        {"id":"Question1",
+        "description":"Most Popular Cloud Platform Today",
+        "correctAnswer":"AWS"}
+    """;
+
+    assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
+    assertEquals("application/json", responseEntity.getHeaders().get("Content-Type").get(0));
+
+    JSONAssert.assertEquals(expectedResponse, responseEntity.getBody(), false);
 
   }
 
+  @Test
+  void retrieveAllSurveyQuestions_basicScenario() throws JSONException {
+
+    ResponseEntity<String> responseEntity = template.getForEntity(GENERIC_QUESTION_URL, String.class);
+
+    String expectedResponse = """
+        [
+          {
+          "id": "Question1"
+          },
+          {
+          "id": "Question2"
+          },
+          {
+          "id": "Question3"
+          }
+        ]
+        """;
+
+    assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
+    assertEquals("application/json", responseEntity.getHeaders().get("Content-Type").get(0));
+
+    JSONAssert.assertEquals(expectedResponse, responseEntity.getBody(), false);
+
+  }
+
+  @Test
+  void addNewSurveyQuestion_basicScenario() {
+
+    String requestBody = """
+        {
+          "description": "Your favourite language",
+          "options": [
+          "Java",
+          "Python",
+          "Typescript",
+          "C++"
+          ],
+          "correctAnswer": "Jav"
+          }
+
+          """;
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.add("Content-Type", "application/json");
+
+    HttpEntity<String> httpEntity = new HttpEntity<String>(requestBody, headers);
+
+    ResponseEntity<String> responseEntity = template.exchange(GENERIC_QUESTION_URL, HttpMethod.POST, httpEntity,
+        String.class);
+
+    assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
+
+    assertTrue(responseEntity.getHeaders().get("Location").get(0).contains("/surveys/Survey1/questions/"));
+
+  }
 
 }
